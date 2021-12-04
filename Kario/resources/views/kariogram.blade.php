9dbd773a84ea375a477bbbb7deadf445e2622b9b
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2021-12-04 08:47:08 +0700

    Tabel dalam card

--- Kario/resources/views/kariogram.blade.php
+++ Kario/resources/views/kariogram.blade.php
@@ -26,64 +26,74 @@ tr:nth-child(even) {
 </style>
 </head>
 <body>
-
-        <table>
-        <tr>
-            <th>Pertanyaan</th>
-            <th>Parameter</th>
-            <th>Nilai</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Caries Experience</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Related Disease</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Diet Contents</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>Diet Frequency</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>Plaque Amount</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>6</td>
-            <td>Mutans Streptococci</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>7</td>
-            <td>Fluoride Program</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>8</td>
-            <td>Saliva Secretion</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>9</td>
-            <td>Saliva Secretion</td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        <tr>
-            <td>10</td>
-            <td>Clinical Judgement/td>
-            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
-        </tr>
-        </table>
+<div class="container p-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Form</div>
+                 <div class="card-body">
+                        <table>
+                        <tr>
+                            <th>Pertanyaan</th>
+                            <th>Parameter</th>
+                            <th>Nilai</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Caries Experience</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Related Disease</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Diet Contents</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>Diet Frequency</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>Plaque Amount</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>Mutans Streptococci</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>Fluoride Program</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>8</td>
+                            <td>Saliva Secretion</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Saliva Secretion</td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>Clinical Judgement/td>
+                            <td><label for="points">Points (between 1 to 5):</label><input type="range" id="points" name="points" min="1" max="5"></td>
+                        </tr>
+                        </table>
+                 </div>
+            </div>
+        </div>
+    </div>
+</div>
 
         </body>
     </html>
